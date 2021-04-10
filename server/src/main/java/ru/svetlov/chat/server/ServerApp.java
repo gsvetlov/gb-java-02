e1d71ee1fd45c8a72943f331a41915843753929a
Georg Svetlov <gsvetlov@gmail.com>
2021-04-11 00:39:26 +0300

    java#3 hw#6 minor cleanup

--- server/src/main/java/ru/svetlov/chat/server/ServerApp.java
+++ server/src/main/java/ru/svetlov/chat/server/ServerApp.java
@@ -10,7 +10,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class ServerApp implements Loggable {
+public class ServerApp {
     private static final Logger log = LogManager.getLogger();
     static Server server;
     static AuthenticationProvider provider;
